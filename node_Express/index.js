6e6c3d898a724b0ee1071d5e2b62abb47c805e07
Ascript <luisrios.lar@gmail.com>
2024-06-04 00:29:38 -0500

    express algorithm

--- node_Express/index.js
+++ node_Express/index.js
@@ -1,37 +1,18 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
 const port = 12000;
+const data = require('../createFile/data.json');
 
-const filePath = path.join(__dirname, '../createFile/data.json');
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
-
-  let jsonData = '';
-
-  readStream.on('data', (chunk) => {
-    jsonData += chunk;
-  });
-
-  readStream.on('end', () => {
-    try {
-      const parsedData = JSON.parse(jsonData);
-      console.log('Number of records:', parsedData.length);
-      res.json(parsedData);
-    } catch (parseErr) {
-      console.error('Error parsing JSON:', parseErr);
-      res.json({ error: true });
-    }
-  });
-
-  readStream.on('error', (err) => {
-    console.error('Error reading file:', err);
-    res.json({ error: true });
-  });
+app.post('/findOne', (req, res) => {
+  res.json(findOne(req.body.id))
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function findOne(id) {
+  return data.filter(rcd => rcd.id === id)?.[0] || {}
+}
