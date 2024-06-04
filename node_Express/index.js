f042d0bd93536aecbad4c6411ace5ff3b44d6427
Ascript <luisrios.lar@gmail.com>
2024-06-04 07:45:21 -0500

    get all data with express

--- node_Express/index.js
+++ node_Express/index.js
@@ -9,10 +9,18 @@ app.post('/findOne', (req, res) => {
   res.json(findOne(req.body.id))
 });
 
+app.post('/findTen', (req, res) => {
+  res.json(find10(req.body.ids))
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 function findOne(id) {
   return data.filter(rcd => rcd.id === id)?.[0] || {}
+}
+
+function find10(ids) {
+  return data.filter(rcd => ids.includes(rcd.id))
 }
